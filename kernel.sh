878e8a0f92d4ec68c1d38188cf11516c91f20ae7
Nageen Chand <25462010+archie9211@users.noreply.github.com>
2020-04-01 16:39:00 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -27,7 +27,7 @@ set -euo pipefail
 
 # The defult directory where the kernel should be placed
 KERNEL_DIR=$PWD
-
+apt install gcc-aarch64-linux-gnu
 # The name of the Kernel, to name the ZIP
 ZIPNAME="DarkOne"
 
