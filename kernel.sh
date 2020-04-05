87baaa88d31b2c1a43deab968a33393e41359c36
Nageen Chand <25462010+archie9211@users.noreply.github.com>
2020-04-05 13:59:34 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -20,7 +20,6 @@
 
 #Kernel building script
 
-set -euo pipefail
 
 ##------------------------------------------------------##
 ##----------Basic Informations, COMPULSORY--------------##
@@ -44,33 +43,13 @@ DEFCONFIG=chef_defconfig
 INCREMENTAL=0
 
 # Push ZIP to Telegram. 1 is YES | 0 is NO(default)
-PTTG=1
-	if [ $PTTG == 1 ]
-	then
-		# Set Telegram Chat ID
-		CHATID="871884164"
-	fi
 
-# Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
-DEF_REG=0
+CHATID="871884164"
+
+
 
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
-BUILD_DTBO=0
-
-# Sign the zipfile
-# 1 is YES | 0 is NO
-SIGN=0
-	if [ $SIGN == 1 ]
-	then
-			#Check java installed or not
-			JAVAEXEC="/usr/bin/java"
-			if [ ! -d $JAVAEXEC ]
-			then
-				# install java 8 via aptitude
-				apt-get -y install openjdk-8-jdk;
-			fi
-	fi
 
 # Check if we are using a dedicated CI ( Continuous Integration ), and
 # set KBUILD_BUILD_VERSION
@@ -95,7 +74,15 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 
 function clone {
 	echo " "
-	export KBUILD_COMPILER_STRING="aarch64-linux-gnu-gcc by GNU"
+	echo "★★Cloning clang 10"
+	wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r377782c.tar.gz
+	mkdir clang
+	tar xvzf clang-r377782c.tar.gz --directory=clang
+	# Toolchain Directory defaults to clang-llvm
+	TC_DIR=$PWD/clang
+	echo "★★Clang Done, Now Its time for AnyKernel .."
+	git clone --depth 1 --no-single-branch https://github.com/archie9211/AnyKernel2.git
+	echo "★★Cloning Kinda Done..!!!"
 }
 
 ##------------------------------------------------------##
@@ -105,9 +92,13 @@ function exports {
 	export KBUILD_BUILD_HOST="circleci"
 	export ARCH=arm64
 	export SUBARCH=arm64
+	KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	export KBUILD_COMPILER_STRING
+	PATH=$TC_DIR/bin/:$PATH
+	export PATH
 	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
 	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
-	export PROCS=$((nproc --all)*(90/100))
+	export PROCS=$(nproc --all)
 }
 
 ##---------------------------------------------------------##
@@ -133,35 +124,18 @@ function tg_post_build {
 ##----------------------------------------------------------##
 
 function build_kernel {
-	if [ $INCREMENTAL == 0 ]
-	then
-		make clean && make mrproper && rm -rf out
-	fi
 
-	if [ "$PTTG" == 1 ]
- 	then
-		tg_post_msg "<b>$CIRCLE_BUILD_NUM CI Build Triggered</b>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>CircleCI</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CIRCLE_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
-	fi
+	tg_post_msg "<b>$CIRCLE_BUILD_NUM CI Build Triggered</b>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>CircleCI</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CIRCLE_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
+
 
 	make O=out $DEFCONFIG
-	if [ $DEF_REG == 1 ]
-	then
-		cp .config arch/arm64/configs/$DEFCONFIG
-		git add arch/arm64/configs/$DEFCONFIG
-		git commit -m "$DEFCONFIG: Regenerate
-								This is an auto-generated commit"
-	fi
 
 	BUILD_START=$(date +"%s")
 	make -j$PROCS O=out \
-		CROSS_COMPILE="aarch64-linux-gnu-" 2>&1 | tee error.log
-		
-	if [ $BUILD_DTBO == 1 ]
-	then
-		tg_post_msg "Building DTBO.." "$CHATID"
-		python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
-			create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
-	fi
+		CROSS_COMPILE=aarch64-linux-gnu- \
+		CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+		CC=clang  2>&1 | tee error.log
+	
 	BUILD_END=$(date +"%s")
 	DIFF=$((BUILD_END - BUILD_START))
 	check_img
@@ -173,11 +147,8 @@ function check_img {
 	if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ] 
 	    then
 		gen_zip
-	else
-		if [ "$PTTG" == 1 ]
- 		then
-			tg_post_build "error.log" "$CHNLID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
-		fi
+	else		
+		tg_post_build "error.log" "$CHNLID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
 	fi
 }
 
@@ -185,29 +156,10 @@ function check_img {
 
 function gen_zip {
 	mv $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
-	if [ $BUILD_DTBO == 1 ]
-	then
-		mv $KERNEL_DIR/out/arch/arm64/boot/dtbo.img AnyKernel2/dtbo.img
-	fi
 	cd AnyKernel2
-	zip -r9 $ZIPNAME-$DEVICE-$DATE * -x .git README.md
-
-	if [ $SIGN == 1 ]
-	then
-		## Sign the zip before sending it to telegram
-		if [ "$PTTG" == 1 ]
- 		then
- 			tg_post_msg "Signing Zip file with AOSP keys.." "$CHATID"
- 		fi
-		curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-		java -jar zipsigner-3.0.jar $ZIPNAME-$DEVICE-$DATE.zip $ZIPNAME-$DEVICE-$DATE-signed.zip
-	fi
-
-	MD5CHECK=$(md5sum $ZIPNAME-$DEVICE-$DATE-signed.zip | cut -d' ' -f1)
-	if [ "$PTTG" == 1 ]
- 	then
-		tg_post_build $ZIPNAME-$DEVICE-$DATE-signed.zip "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) | MD5 Checksum : <code>$MD5CHECK</code>"
-	fi
+	zip -r9 $ZIPNAME-$DEVICE-$DATE * -x .git README.md	
+	MD5CHECK=$(md5sum $ZIPNAME-$DEVICE-$DATE.zip | cut -d' ' -f1)	
+	tg_post_build $ZIPNAME-$DEVICE-$DATE.zip "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) | MD5 Checksum : <code>$MD5CHECK</code>"
 	cd ..
 }
 
